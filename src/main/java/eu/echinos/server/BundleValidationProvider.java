8fe0c34a713e6a1800dad63e46ecfc027ca34f8b
patrick-werner <pa.f.werner@gmail.com>
2023-09-22 13:04:24 +0200

    BundleValidationProvider added

---
+++ src/main/java/eu/echinos/server/BundleValidationProvider.java
@@ -0,0 +1,38 @@
+package eu.echinos.server;
+
+import ca.uhn.fhir.rest.annotation.ResourceParam;
+import ca.uhn.fhir.rest.annotation.Validate;
+import ca.uhn.fhir.rest.api.MethodOutcome;
+import ca.uhn.fhir.rest.server.IResourceProvider;
+import ca.uhn.fhir.validation.ValidationResult;
+import eu.echinos.validator.ValidationProvider;
+import org.hl7.fhir.instance.model.api.IBaseResource;
+import org.hl7.fhir.r4.model.Bundle;
+import org.hl7.fhir.r4.model.OperationOutcome;
+
+public class BundleValidationProvider implements IResourceProvider {
+
+  private final ValidationProvider validationProvider;
+
+  @Override
+  public Class<? extends IBaseResource> getResourceType() {
+    return Bundle.class;
+  }
+
+  @Validate
+  public MethodOutcome validateBundle(@ResourceParam Bundle bundle) {
+    final MethodOutcome outcome = new MethodOutcome();
+
+    ValidationResult validationResult = validationProvider.validateWithResult(bundle);
+    OperationOutcome operationOutcome = (OperationOutcome) validationResult.toOperationOutcome();
+
+    //TODO: filter Warnings etc....
+
+    outcome.setOperationOutcome(operationOutcome);
+    return outcome;
+  }
+
+  public BundleValidationProvider(final ValidationProvider validationProvider) {
+    this.validationProvider = validationProvider;
+  }
+}
